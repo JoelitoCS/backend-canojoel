6462548ac2dccb940828e5b694837b868c84c4e1
JoelitoCS <canosanchezjoel@fpllefia.com>
2026-02-08 14:10:38 +0100

    mas cambios en index.js

--- src/api/index.js
+++ src/api/index.js
@@ -22,6 +22,8 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => { res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL); res.setHeader('Access-Control-Allow-Credentials', 'true'); res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); next(); });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
